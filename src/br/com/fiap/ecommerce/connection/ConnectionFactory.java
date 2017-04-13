226e3b1dde1921059eaef8d652c2c325e1959b45
Wendler Eis <wendler_eis@hotmail.com>
2017-04-12 22:43:43 -0300

    Add database connection class

--- src/br/com/fiap/ecommerce/connection/ConnectionFactory.java
+++ src/br/com/fiap/ecommerce/connection/ConnectionFactory.java
@@ -1,5 +1,29 @@
 package br.com.fiap.ecommerce.connection;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionFactory {
+	private static Connection connection = null;
+	private static String url = "";
+	private static String user = "";
+	private static String pass = "";
+	
+	public static Connection getConnection(){
+		if (connection == null) {
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				connection = DriverManager.getConnection(url, user, pass);
+			} 
+			catch (SQLException e) {
+				System.out.println("Erro Conexão com Banco de Dados - Detalhes: " + e);
+			}
+			catch (ClassNotFoundException e) {
+				System.out.println("Erro Carregamento Driver Conexão - Detalhes: " + e);
+			}
+		}
+		
+		return connection;		
+	}
 }
