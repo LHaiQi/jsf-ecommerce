d86d24aebc1d0658291904a726390fcc23c1acd2
Wendler Eis <wendler_eis@hotmail.com>
2017-05-11 00:28:47 -0300

    Added publisher error on screen

--- src/br/com/fiap/ecommerce/dao/PublisherDAO.java
+++ src/br/com/fiap/ecommerce/dao/PublisherDAO.java
@@ -16,168 +16,161 @@ public class PublisherDAO {
 	private ResultSet resultSet;
 	private String sql;
 	
-	public PublisherBean getPublisher(PublisherBean publisher){
+	public int generateID(){
+		int publisherID = 1;
+		
+		connection = ConnectionFactory.getConnection();
+		sql = "Select Max(publisherID) as publisherID From publisher";
+		
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next()){
+				publisherID = resultSet.getInt("publisherID") + 1;
+			}			
+		} 
+		catch (Exception e) {
+			publisherID = 1;
+		}
+		
+		return publisherID;
+	}
+	
+	public PublisherBean getPublisher(PublisherBean publisher) throws SQLException{
 		PublisherBean newPublisher = null;
 		
 		connection = ConnectionFactory.getConnection();
 		sql = "Select * From Publisher Where PublisherID = ?";
+			
+		preparedStatement = connection.prepareStatement(sql);
+		preparedStatement.setInt(1, publisher.getId());
 		
-		try {
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, publisher.getId());
+		resultSet = preparedStatement.executeQuery();
+		
+		if (resultSet.next()) {
+			int id = resultSet.getInt("PublisherID");
+			String publisherName = resultSet.getString("Publisher");
+			int cnpj = resultSet.getInt("cnpj");
+			String email = resultSet.getString("Email");;
+			int phoneNumber = resultSet.getInt("PhoneNumber");
+			String country = resultSet.getString("Country");;
+			String state = resultSet.getString("cState");;
+			String street = resultSet.getString("Street");;
+			int zipCode = resultSet.getInt("ZipCode");
+			int addressNumber = resultSet.getInt("AddressNumber");
 			
-			resultSet = preparedStatement.executeQuery();
-			
-			if (resultSet.next()) {
-				int id = resultSet.getInt("PublisherID");
-				String publisherName = resultSet.getString("Publisher");
-				int cnpj = resultSet.getInt("cnpj");
-				String email = resultSet.getString("Email");;
-				int phoneNumber = resultSet.getInt("PhoneNumber");
-				String country = resultSet.getString("Country");;
-				String state = resultSet.getString("cState");;
-				String street = resultSet.getString("Street");;
-				int zipCode = resultSet.getInt("ZipCode");
-				int addressNumber = resultSet.getInt("AddressNumber");
-				
-				newPublisher = new PublisherBean(id, publisherName, cnpj, email, phoneNumber, country, state, street, zipCode, addressNumber);
-			}
-		} catch (Exception e) {
-			System.out.println("Erro ao buscar editora: " + e);
+			newPublisher = new PublisherBean(id, publisherName, cnpj, email, phoneNumber, country, state, street, zipCode, addressNumber);
 		}
 	
 		return newPublisher;
 	}
 	
-	public List<PublisherBean> getAllPublishers() {
+	public List<PublisherBean> getAllPublishers() throws SQLException {
 		List<PublisherBean> listPublishers = new ArrayList<PublisherBean>();
 		
 		connection = ConnectionFactory.getConnection();
 		sql = "Select * From Publisher";
 		
-		try {
-			preparedStatement = connection.prepareStatement(sql);
-			resultSet = preparedStatement.executeQuery();
+		preparedStatement = connection.prepareStatement(sql);
+		resultSet = preparedStatement.executeQuery();
+		
+		while (resultSet.next()) {
+			int id = resultSet.getInt("PublisherID");
+			String publisherName = resultSet.getString("Publisher");
+			int cnpj = resultSet.getInt("cnpj");
+			String email = resultSet.getString("Email");;
+			int phoneNumber = resultSet.getInt("PhoneNumber");
+			String country = resultSet.getString("Country");;
+			String state = resultSet.getString("cState");;
+			String street = resultSet.getString("Street");;
+			int zipCode = resultSet.getInt("ZipCode");
+			int addressNumber = resultSet.getInt("AddressNumber");
 			
-			while (resultSet.next()) {
-				int id = resultSet.getInt("PublisherID");
-				String publisherName = resultSet.getString("Publisher");
-				int cnpj = resultSet.getInt("cnpj");
-				String email = resultSet.getString("Email");;
-				int phoneNumber = resultSet.getInt("PhoneNumber");
-				String country = resultSet.getString("Country");;
-				String state = resultSet.getString("cState");;
-				String street = resultSet.getString("Street");;
-				int zipCode = resultSet.getInt("ZipCode");
-				int addressNumber = resultSet.getInt("AddressNumber");
-				
-				listPublishers.add(new PublisherBean(id, publisherName, cnpj, email, phoneNumber, country, state, street, zipCode, addressNumber));							
-			}
-		} catch (Exception e) {
-			System.out.println("Erro ao buscar lista editora: " + e);
+			listPublishers.add(new PublisherBean(id, publisherName, cnpj, email, phoneNumber, country, state, street, zipCode, addressNumber));							
 		}
 		
 		return listPublishers;
 	}
 	
-	public List<PublisherBean> getAllPublishers(PublisherBean publisher){
+	public List<PublisherBean> getAllPublishers(PublisherBean publisher) throws SQLException{
 		List<PublisherBean> listPublishers = new ArrayList<PublisherBean>();
 		
 		connection = ConnectionFactory.getConnection();
 		sql = "Select * From Publisher Where Publisher Like ?";
 		
-		try {
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, "%" + publisher.getPublisher() + "%");
+		preparedStatement = connection.prepareStatement(sql);
+		preparedStatement.setString(1, "%" + publisher.getPublisher() + "%");
+		
+		resultSet = preparedStatement.executeQuery();
+		
+		while (resultSet.next()) {
+			int id = resultSet.getInt("PublisherID");
+			String publisherName = resultSet.getString("Publisher");
+			int cnpj = resultSet.getInt("cnpj");
+			String email = resultSet.getString("Email");;
+			int phoneNumber = resultSet.getInt("PhoneNumber");
+			String country = resultSet.getString("Country");;
+			String state = resultSet.getString("cState");;
+			String street = resultSet.getString("Street");;
+			int zipCode = resultSet.getInt("ZipCode");
+			int addressNumber = resultSet.getInt("AddressNumber");
 			
-			resultSet = preparedStatement.executeQuery();
-			
-			while (resultSet.next()) {
-				int id = resultSet.getInt("PublisherID");
-				String publisherName = resultSet.getString("Publisher");
-				int cnpj = resultSet.getInt("cnpj");
-				String email = resultSet.getString("Email");;
-				int phoneNumber = resultSet.getInt("PhoneNumber");
-				String country = resultSet.getString("Country");;
-				String state = resultSet.getString("cState");;
-				String street = resultSet.getString("Street");;
-				int zipCode = resultSet.getInt("ZipCode");
-				int addressNumber = resultSet.getInt("AddressNumber");
-				
-				listPublishers.add(new PublisherBean(id, publisherName, cnpj, email, phoneNumber, country, state, street, zipCode, addressNumber));							
-			}
-		} catch (Exception e) {
-			System.out.println("Erro ao buscar lista editora: " + e);
+			listPublishers.add(new PublisherBean(id, publisherName, cnpj, email, phoneNumber, country, state, street, zipCode, addressNumber));							
 		}
 		
 		return listPublishers;
 	}
 
-	public void setPublisher(PublisherBean publisherBean) {
+	public void setPublisher(PublisherBean publisherBean) throws SQLException {
 		connection = ConnectionFactory.getConnection();
-		sql = "Insert Into Publisher Values ((select max(publisherId)+1 from publisher),?,?,?,?,?,?,?,?,?)";
+		sql = "Insert Into Publisher(PublisherID, Publisher, CNPJ, Email, PhoneNumber, Country, CState, Street, ZipCode, AddressNumber) "
+				+ "Values (?,?,?,?,?,?,?,?,?,?)";
+						
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);		
+		preparedStatement.setInt(1, generateID());
+		preparedStatement.setString(2, publisherBean.getPublisher());
+		preparedStatement.setInt(3, publisherBean.getCnpj());
+		preparedStatement.setString(4, publisherBean.getEmail());
+		preparedStatement.setInt(5, publisherBean.getPhoneNumber());
+		preparedStatement.setString(6, publisherBean.getCountry());
+		preparedStatement.setString(7, publisherBean.getState());
+		preparedStatement.setString(8, publisherBean.getStreet());
+		preparedStatement.setInt(9, publisherBean.getZipCode());
+		preparedStatement.setInt(10, publisherBean.getAddressNumber());
 		
-		try {
-			preparedStatement = connection.prepareStatement(sql);
-			
-			preparedStatement.setString(1, publisherBean.getPublisher());
-			preparedStatement.setInt(2, publisherBean.getCnpj());
-			preparedStatement.setString(3, publisherBean.getEmail());
-			preparedStatement.setInt(4, publisherBean.getPhoneNumber());
-			preparedStatement.setString(5, publisherBean.getCountry());
-			preparedStatement.setString(6, publisherBean.getState());
-			preparedStatement.setString(7, publisherBean.getStreet());
-			preparedStatement.setInt(8, publisherBean.getZipCode());
-			preparedStatement.setInt(9, publisherBean.getAddressNumber());
-			
-			preparedStatement.execute();
-			
-		} catch (SQLException e) {
-			System.out.println("Erro ao inserir editor(a): " + e);
-
-		}
-		
+		preparedStatement.execute();	
 	}
 	
-	public void deletePublisher(PublisherBean publisherBean){
+	public void deletePublisher(PublisherBean publisherBean) throws SQLException{
 		connection = ConnectionFactory.getConnection();
 		sql = "DELETE FROM PUBLISHER WHERE PUBLISHERID = ?";
+				
+		preparedStatement = connection.prepareStatement(sql);
 		
-		try {
-			preparedStatement = connection.prepareStatement(sql);
-			
-			preparedStatement.setInt(1, publisherBean.getId());
-			
-			preparedStatement.execute();
-			
-		} catch (SQLException e) {
-			System.out.println("Erro ao apagar editor(a): " + e);
-		}
+		preparedStatement.setInt(1, publisherBean.getId());
+		
+		preparedStatement.execute();
 	}
 
-	public void alterPublisher(PublisherBean publisher) {
+	public void alterPublisher(PublisherBean publisher) throws SQLException {
 		connection = ConnectionFactory.getConnection();
 		sql = "Update Publisher set Publisher = ?, Cnpj = ?, Email = ?,"
 			+ " PhoneNumber = ?, Country = ?, cState = ?, Street = ?,"
 			+ " ZipCode = ?, AddressNumber = ? Where PublisherID = ?";
 		
-		try {
-			PreparedStatement ps = connection.prepareStatement(sql);
-			ps.setString(1, publisher.getPublisher());
-			ps.setInt(2, publisher.getCnpj());
-			ps.setString(3, publisher.getEmail());
-			ps.setInt(4, publisher.getPhoneNumber());
-			ps.setString(5, publisher.getCountry());
-			ps.setString(6, publisher.getState());
-			ps.setString(7, publisher.getStreet());
-			ps.setInt(8, publisher.getZipCode());
-			ps.setInt(9, publisher.getAddressNumber());
-			ps.setInt(10, publisher.getId());
-			
-			ps.execute();
-			
-		} catch (Exception e) {
-			System.out.println("Erro ao Editar editora: " + e);
-		}
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ps.setString(1, publisher.getPublisher());
+		ps.setInt(2, publisher.getCnpj());
+		ps.setString(3, publisher.getEmail());
+		ps.setInt(4, publisher.getPhoneNumber());
+		ps.setString(5, publisher.getCountry());
+		ps.setString(6, publisher.getState());
+		ps.setString(7, publisher.getStreet());
+		ps.setInt(8, publisher.getZipCode());
+		ps.setInt(9, publisher.getAddressNumber());
+		ps.setInt(10, publisher.getId());
+		
+		ps.execute();
 	}	
 }
