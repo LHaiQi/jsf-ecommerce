ad3ddf21e82745a2793ec7dbd62faa646271cf9b
Wendler Eis <wendler_eis@hotmail.com>
2017-05-20 20:17:17 -0300

    Added Wishlist flow

---
+++ src/br/com/fiap/ecommerce/bean/WishlistBean.java
@@ -0,0 +1,24 @@
+package br.com.fiap.ecommerce.bean;
+
+public class WishlistBean {
+	private BookBean book;
+	private UserBean user;
+	
+	public WishlistBean(BookBean book, UserBean user) {
+		this.book = book;
+		this.user = user;
+	}
+	
+	public BookBean getBook() {
+		return book;
+	}
+	public void setBook(BookBean book) {
+		this.book = book;
+	}
+	public UserBean getUser() {
+		return user;
+	}
+	public void setUser(UserBean user) {
+		this.user = user;
+	}
+}
---
+++ src/br/com/fiap/ecommerce/bo/WishlistBO.java
@@ -0,0 +1,25 @@
+package br.com.fiap.ecommerce.bo;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.fiap.ecommerce.bean.WishlistBean;
+import br.com.fiap.ecommerce.dao.WishlistDAO;
+
+public class WishlistBO {
+
+	public void setWishlist(WishlistBean wishlist) throws SQLException {
+		WishlistDAO wishlistDAO = new WishlistDAO();
+		wishlistDAO.setWishlist(wishlist);
+	}
+
+	public List<WishlistBean> getAllWishes(WishlistBean wishlist) throws SQLException {
+		WishlistDAO wishlistDAO = new WishlistDAO();
+		return wishlistDAO.getAllWishes(wishlist);
+	}
+
+	public void deleteWishItem(WishlistBean wishlist) throws SQLException {
+		WishlistDAO wishlistDAO = new WishlistDAO();
+		wishlistDAO.deleteWishItem(wishlist);
+	}
+}
